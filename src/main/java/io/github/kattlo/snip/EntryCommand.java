cbce3d350f44d4dbd860d22ff981446633e10ae9
Fábio José <fabiojose@gmail.com>
2021-03-19 17:55:44 -0300

    Version provider for quarkus
    Signed-off-by: Fábio José <fabiojose@gmail.com>

--- src/main/java/io/github/kattlo/snip/EntryCommand.java
+++ src/main/java/io/github/kattlo/snip/EntryCommand.java
@@ -1,5 +1,6 @@
 package io.github.kattlo.snip;
 
+import io.github.kattlo.util.VersionUtil;
 import io.quarkus.picocli.runtime.annotations.TopCommand;
 import picocli.CommandLine;
 
@@ -9,6 +10,7 @@ import picocli.CommandLine;
 @TopCommand
 @CommandLine.Command(
     name = "snip",
+    versionProvider = VersionUtil.QuarkusVersionProvider.class,
     mixinStandardHelpOptions = true,
     subcommands = {
         CreateCommand.class
