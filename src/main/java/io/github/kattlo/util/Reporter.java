37a283b11bc017d9a3193e676f56c3972e753e4a
Fábio José <fabiojose@gmail.com>
2021-03-24 06:47:25 -0300

    impl to report the result of executions
    Signed-off-by: Fábio José <fabiojose@gmail.com>

---
+++ src/main/java/io/github/kattlo/util/Reporter.java
@@ -0,0 +1,39 @@
+package io.github.kattlo.util;
+
+import java.io.PrintStream;
+
+/**
+ * @author fabiojose
+ */
+public class Reporter {
+
+    private PrintStream out;
+
+    private Reporter() {}
+
+    public static Reporter create() {
+
+        var reporter = new Reporter();
+        reporter.out = System.out;
+
+        return reporter;
+    }
+
+    public void message(String message) {
+
+        out.println();
+        out.print(" > > ");
+        out.print(message);
+        out.println();
+        out.println();
+
+    }
+
+    public void success(String message){
+        message("[SUCCESS] " + message);
+    }
+
+    public void failure(String message){
+        message("[FAIL] " + message);
+    }
+}
